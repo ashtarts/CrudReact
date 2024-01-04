b0b1970529e1a927023be03be0816cc5bc21fcfe
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-04 18:40:38 -0300

    header

--- Front-end/src/components/Header.jsx
+++ Front-end/src/components/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import '../styles/Header.sass';
+import { RiShoppingCartLine } from "react-icons/ri";
 
 const Header = () => {
   return (
-    <div>E-commerce</div>
+    <header>
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap" rel="stylesheet"></link>
+        <div className="logo-container">
+            <div className="icon"><RiShoppingCartLine /></div>
+            <h1 className='logo'>Nunes Sports</h1>
+        </div>
+    </header>
   )
 }
 
