1fcf6ce1b629459655b7ca1c06da522c1080ee0d
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-05 14:31:35 -0300

    deletar

--- Front-end/src/pages/delete/Delete.jsx
+++ Front-end/src/pages/delete/Delete.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import '../delete/Delete.sass';
 
-const Delete = () => {
+const handleKeyPress = (event) => { /* para utilizar o enter como submit */
+    if (event.key === 'Enter') {
+      event.preventDefault(); 
+    }
+};
+
+const Delete = ({algumaFuncaoFutura}) => {
   return (
-    <h3>Products Menu</h3>
+    <div className="delete-container">
+      <h3 className='delete-title'>Products Menu</h3>
+      <form onSubmit={algumaFuncaoFutura}>
+        <div className='delete-field'>
+          <label className="field-description">Informe o código do produto:</label>
+          <input className = "field" type="text" 
+          required
+          onKeyUp={handleKeyPress}
+          /> 
+        </div>
+        <button className="button" onClick={algumaFuncaoFutura}>
+            <span className="lable">Deletar</span>
+        </button>
+      </form>
+    </div>
   )
 }
 
--- Front-end/src/pages/update/Update.jsx
+++ Front-end/src/pages/update/Update.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import '../update/Update.sass';
 
-const Update = () => {
+const handleKeyPress = (event) => { /* para utilizar o enter como submit */
+    if (event.key === 'Enter') {
+      event.preventDefault(); 
+    }
+};
+
+const Update = (algumaFuncaoFutura) => {
   return (
-    <h3>Products Menu</h3>
+    <div className="update-container">
+      <h3 className='update-title'>Products Menu</h3>
+      <form onSubmit={algumaFuncaoFutura}>
+        <div className='update-field'>
+          <label className="field-description">Informe o código do produto a ser deletado:</label>
+          <input className = "field" type="text" 
+          required
+          onKeyUp={handleKeyPress}
+          /> 
+        </div>
+        <button className="button" onClick={algumaFuncaoFutura}>
+          <span className="lable">Enviar</span>
+        </button>
+      </form>
+    </div>
   )
 }
 
