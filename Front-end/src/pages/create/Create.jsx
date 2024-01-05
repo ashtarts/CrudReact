68f4bb09ed3bda1f24a0d6d677a5bfa525d184d0
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-05 17:13:29 -0300

    formulário implementado

--- Front-end/src/pages/create/Create.jsx
+++ Front-end/src/pages/create/Create.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import '../create/Create.sass';
 
-const handleKeyPress = (event) => { /* para utilizar o enter como submit */
-    if (event.key === 'Enter') {
-      event.preventDefault(); 
-    }
-};
-
 const Create = ({algumaFuncaoFutura}) => {
+  const [values, setValues] = useState({});// as chaves para represntar um objeto
+
+  const handleValues = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value
+    })
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Simulação do envio dos dados
+    console.log(values);
+    setValues({
+      name: '',
+      code: '',
+      description: '',
+      price: ''
+    });
+  };
   return (
     <div className="create-container">
       <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap" rel="stylesheet"></link>
@@ -15,33 +31,51 @@ const Create = ({algumaFuncaoFutura}) => {
 
       <div className="fields">
 
-        <form onSubmit={algumaFuncaoFutura}>
+        <form onSubmit={handleSubmit}>
           <div className='create-field'>
             <label className="field-description">Nome do produto:</label>
             <input className = "field" type="text" 
             required
-            onKeyUp={handleKeyPress}
+            name='name'
+            onChange={handleValues}
+            value={values.name}
             />
           </div>
+
           <div className='create-field'>
             <label className="field-description">Código do produto:</label>
             <input className = "field" type="text" 
             required
-            onKeyUp={handleKeyPress}/>
+            name='code'
+            onChange={handleValues}
+            value={values.code}
+            />
           </div>
+
           <div className='create-field'>
             <label className="field-description">Descrição do produto:</label>
             <input className = "field" type="text"
             required
-            onKeyUp={handleKeyPress}/>
+            name='description'
+            onChange={handleValues}
+            value={values.description}
+            />
           </div>
+
           <div className='create-field'>
             <label className="field-description">Preço do produto:</label>
             <input className = "field" type="text"
             required
-            onKeyUp={handleKeyPress}/>
+            name='price'
+            onChange={handleValues}
+            value={values.price}
+            />
+            
           </div>
-          <button className="button" onClick={algumaFuncaoFutura}>
+
+          <button type="submit" // ao utilizar type ="submit" no botão, o formulário é enviado com enter
+                className="button" 
+                onClick={handleSubmit}> 
               <span className="lable">Enviar</span>
           </button>
         </form>
