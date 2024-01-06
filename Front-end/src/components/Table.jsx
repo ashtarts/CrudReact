c5958a57010c7090b3608ffcac9720181e2c2f6c
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-06 11:37:20 -0300

    table

--- Front-end/src/components/Table.jsx
+++ Front-end/src/components/Table.jsx
@@ -1,34 +1,17 @@
 import React from 'react'
 import '../styles/Table.sass';
 
-const Table = () => {
+const Table = ({itens}) => {
   return (
     <table>
-      <tr>
-        <td>Thinkertoys: A Handbook of Creative-Thinking Techniques</td>
-        <td>Michael Michalko</td>
-        <td>$19.99</td>
-      </tr>
-      <tr>
-        <td>Design Basics</td>
-        <td>Stephen Pentak</td>
-        <td>$173.95</td>
-      </tr>
-      <tr>
-        <td>The Creative Habit: Learn It and Use It for Life</td>
-        <td>Twyla Tharp</td>
-        <td>$17.00</td>
-      </tr>
-      <tr>
-        <td>Design in the USA</td>
-        <td>Jeffrey L. Meikle</td>
-        <td>$39.95</td>
-      </tr>
-        <tr>
-        <td>The Design Process</td>
-        <td>Karl Aspelund</td>
-        <td>$90.00</td>
-      </tr>
+      {itens?.map((item, index) => ( // a interrogação garante que não é exibido se 'itens' for undefined
+        <tr key = {index}>
+          <td>{item.code}</td>
+          <td>{item.name}</td>
+          <td>{item.description}</td>
+          <td>{item.price}</td>
+        </tr>
+      ))}
     </table>
   )
 }
