b0674dce0ded258df8b1ba775cd68d18af755b8b
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-07 20:38:18 -0300

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['firebase'], 
+    include: ['firebase/app', 'firebase/firestore']
   },
   build: {
     rollupOptions: {
