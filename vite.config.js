c8e866cea1117515fdab7c4b5bf7103f672086ee
marcelopetroni <marceloapetroni@hotmail.com>
2024-01-07 21:41:25 -0300

    ultimo ajuste

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import firebasePlugin from 'vite-plugin-firebase';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), firebasePlugin({
-    // mandatory firebase project id
-    projectId: "crud-react-df318",
-    // project directory, i.e. where firebase.json is (defaults to `config.root`)
-  
-    // whether to materialize (write on disk) `.runtimeconfig.json` for functions emulator (defaults to `false`)
-    materializeConfig: true,
-    // emulator targets (defaults to `['hosting', 'functions']`)
-    targets: ['hosting']
-  })],
+  plugins: [react(),],
   optimizeDeps: {
     exclude: ['firebase'], 
     include: ['firebase/app', 'firebase/firestore']
